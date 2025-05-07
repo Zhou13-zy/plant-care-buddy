1040164e01ec09624a2f682cef486a446115a6e8
Zeyu Hou <howardhzy13@gmail.com>
2025-05-07 21:21:42 +0800

    configure cors

--- PlantCareBuddy/PlantCareBuddy.API/Program.cs
+++ PlantCareBuddy/PlantCareBuddy.API/Program.cs
@@ -1,11 +1,22 @@
+// This is the main entry point and configuration file
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
