b59a0e6a1b07df23caf717531852ab044545cd7b
Zeyu Hou <howardhzy13@gmail.com>
2025-05-10 11:36:46 +0800

    configured router / created placeholder pages / setup axios

---
+++ client/src/pages/PlantsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlantsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Plants</h1>
+      <p>List of plants will go here.</p>
+    </div>
+  );
+};
+
+export default PlantsPage;
