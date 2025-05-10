59d282cb25357840b8036efaa9cfe448a460cb89
Zeyu Hou <howardhzy13@gmail.com>
2025-05-10 12:55:05 +0800

    create plant dto and service to get all the plants

---
+++ PlantCareBuddy/PlantCareBuddy.Application/Services/PlantService.cs
@@ -0,0 +1,31 @@
+﻿using PlantCareBuddy.Application.DTOs.Plant;
+using PlantCareBuddy.Application.Interfaces;
+using PlantCareBuddy.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlantCareBuddy.Application.Services
+{
+    public class PlantService : IPlantService
+    {
+        private readonly PlantCareBuddyContext _context;
+
+        public PlantService(PlantCareBuddyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<PlantDto>> GetAllPlantsAsync()
+        {
+            return await _context.Plants
+                .Select(p => new PlantDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Species = p.Species,
+                    Location = p.Location,
+                    HealthStatus = p.HealthStatus.ToString()
+                })
+                .ToListAsync();
+        }
+    }
+}
