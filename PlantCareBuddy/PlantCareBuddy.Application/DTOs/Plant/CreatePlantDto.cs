0c7926dd3e5ef864427665fa64497c6894461716
Zeyu Hou <howardhzy13@gmail.com>
2025-05-11 09:25:23 +0800

    createPlantDto with validation

---
+++ PlantCareBuddy/PlantCareBuddy.Application/DTOs/Plant/CreatePlantDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using PlantCareBuddy.Domain.Enums;
+
+namespace PlantCareBuddy.Application.DTOs.Plant
+{
+    /// <summary>
+    /// DTO for creating a new plant.
+    /// </summary>
+    public class CreatePlantDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Species { get; set; }
+
+        [Required]
+        public DateTime AcquisitionDate { get; set; }
+
+        [MaxLength(200)]
+        public string Location { get; set; }
+
+        [Required]
+        public PlantHealthStatus HealthStatus { get; set; }
+
+        [MaxLength(1000)]
+        public string? Notes { get; set; }
+
+        [MaxLength(500)]
+        public string? PrimaryImagePath { get; set; }
+    }
+}
