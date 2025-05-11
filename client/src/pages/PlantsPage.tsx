28dafbe05fb36c0c0432be82e448560fbe2238c1
Zeyu Hou <howardhzy13@gmail.com>
2025-05-11 21:51:11 +0800

    Update URL with filter parameters

--- client/src/pages/PlantsPage.tsx
+++ client/src/pages/PlantsPage.tsx
@@ -3,6 +3,7 @@ import { getAllPlants, searchPlants } from '../api/plantService';
 import { Plant } from '../models/plant';
 import PlantList from '../components/plants/PlantList';
 import SearchBar from '../components/plants/SearchBar';
+import { useSearchParams } from 'react-router-dom';
 
 const PlantsPage: React.FC = () => {
   const [plants, setPlants] = useState<Plant[]>([]);
@@ -14,11 +15,23 @@ const PlantsPage: React.FC = () => {
     location: ''
   });
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
-    getAllPlants()
-      .then(setPlants)
-      .catch(() => setPlants([]))
-      .finally(() => setLoading(false));
+    const name = searchParams.get('name') || '';
+    const species = searchParams.get('species') || '';
+    const healthStatus = searchParams.get('healthStatus') || '';
+    const location = searchParams.get('location') || '';
+    setFilters({ name, species, healthStatus, location });
+
+    if (name || species || healthStatus || location) {
+      handleSearch({ name, species, healthStatus: healthStatus ? Number(healthStatus) : undefined, location });
+    } else {
+      getAllPlants()
+        .then(setPlants)
+        .catch(() => setPlants([]))
+        .finally(() => setLoading(false));
+    }
   }, []);
 
   const handleSearch = async (filters: {
@@ -28,6 +41,14 @@ const PlantsPage: React.FC = () => {
     location?: string;
   }) => {
     setLoading(true);
+
+    const params: any = {};
+    if (filters.name) params.name = filters.name;
+    if (filters.species) params.species = filters.species;
+    if (filters.healthStatus !== undefined) params.healthStatus = filters.healthStatus;
+    if (filters.location) params.location = filters.location;
+    setSearchParams(params);
+
     try {
       const results = await searchPlants(filters);
       setPlants(results);
