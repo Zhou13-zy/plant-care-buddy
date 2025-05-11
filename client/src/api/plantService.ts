833a9c09a804a9a4b9dec13e070a0f5155c6a2c5
Zeyu Hou <howardhzy13@gmail.com>
2025-05-11 10:35:15 +0800

    implemented create plant api call on axios typed with dto

--- client/src/api/plantService.ts
+++ client/src/api/plantService.ts
@@ -1,9 +1,15 @@
 import api from './axiosConfig';
 import { Plant } from '../models/plant';
+import { CreatePlantDto } from '../models/createPlantDto';
 
 export const getAllPlants = async (): Promise<Plant[]> => {
   const response = await api.get<Plant[]>('/plants');
   return response.data;
 };
 
-// add more methods here later (addPlant, updatePlant, deletePlant, etc.)
+export const addPlant = async (plant: CreatePlantDto) => {
+    const response = await api.post('/plants', plant);
+    return response.data;
+};
+
+// add more methods here later (updatePlant, deletePlant, etc.)
