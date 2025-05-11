38358620038ab5bf7b84edf8842a749066020938
Zeyu Hou <howardhzy13@gmail.com>
2025-05-11 22:35:09 +0800

    Create LINQ extension methods utility class

---
+++ PlantCareBuddy/PlantCareBuddy.Infrastructure/Extensions/QueryableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using PlantCareBuddy.Domain.Entities;
+using PlantCareBuddy.Domain.Enums;
+
+namespace PlantCareBuddy.Infrastructure.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<Plant> WhereNameContainsInsensitive(this IQueryable<Plant> query, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return query;
+            return query.Where(p => p.Name.ToLower().Contains(value.ToLower()));
+        }
+
+        public static IQueryable<Plant> WhereSpeciesContainsInsensitive(this IQueryable<Plant> query, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return query;
+            return query.Where(p => p.Species.ToLower().Contains(value.ToLower()));
+        }
+
+        public static IQueryable<Plant> WhereLocationContainsInsensitive(this IQueryable<Plant> query, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return query;
+            return query.Where(p => p.Location.ToLower().Contains(value.ToLower()));
+        }
+
+        // Health status filter (not a string)
+        public static IQueryable<Plant> WithHealthStatus(
+            this IQueryable<Plant> query,
+            PlantHealthStatus? status)
+        {
+            if (!status.HasValue)
+                return query;
+            return query.Where(p => p.HealthStatus == status.Value);
+        }
+    }
+}
