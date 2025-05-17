f1cf591eecd21ae4ad09367727d356ca1095d2a6
Zeyu Hou <howardhzy13@gmail.com>
2025-05-17 13:34:12 +0800

    Define configuration settings for photo storage in the Infrastructure layer

---
+++ PlantCareBuddy/PlantCareBuddy.Infrastructure/Config/PhotoStorageSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace PlantCareBuddy.Infrastructure.Config
+{
+    public class PhotoStorageSettings
+    {
+        public string StoragePath { get; set; } = "wwwroot/images";
+        public string[] AllowedExtensions { get; set; } = { ".jpg", ".jpeg", ".png" };
+        public long MaxFileSize { get; set; } = 5 * 1024 * 1024; // 5MB
+        public string BaseUrl { get; set; } = "/images";
+    }
+}
