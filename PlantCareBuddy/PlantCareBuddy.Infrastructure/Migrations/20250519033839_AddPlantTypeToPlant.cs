a7c64aa671e1398886846dc5d653397a0b03eb4c
Zeyu Hou <howardhzy13@gmail.com>
2025-05-19 11:39:25 +0800

    add plant type to plant table & ran migration

---
+++ PlantCareBuddy/PlantCareBuddy.Infrastructure/Migrations/20250519033839_AddPlantTypeToPlant.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlantCareBuddy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlantTypeToPlant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlantType",
+                table: "Plants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlantType",
+                table: "Plants");
+        }
+    }
+}
