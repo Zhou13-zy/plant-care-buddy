7bd6725d6eb2ec071b3de5487ed25a22dd1da839
Zeyu Hou <howardhzy13@gmail.com>
2025-05-19 11:28:47 +0800

    create season enum for aus

---
+++ PlantCareBuddy/PlantCareBuddy.Domain/Enums/Season.cs
@@ -0,0 +1,10 @@
+﻿namespace PlantCareBuddy.Domain.Enums
+{
+    public enum Season
+    {
+        Summer,    // December-February
+        Autumn,    // March-May
+        Winter,    // June-August
+        Spring     // September-November
+    }
+}
