d07ed7c46b60d359e52d06b740d59303ac0b1da7
Zeyu Hou <howardhzy13@gmail.com>
2025-05-25 09:45:26 +0800

    create strategy based reminder service

---
+++ PlantCareBuddy/PlantCareBuddy.Application/Interfaces/IStrategyBasedReminderService.cs
@@ -0,0 +1,14 @@
+using PlantCareBuddy.Domain.Entities;
+
+namespace PlantCareBuddy.Application.Interfaces
+{
+    public interface IStrategyBasedReminderService
+    {
+        /// <summary>
+        /// Generate a set of recommended reminders for a plant based on its care strategy.
+        /// </summary>
+        /// <param name="plant">The plant to generate reminders for.</param>
+        /// <returns>A list of generated Reminder entities.</returns>
+        IEnumerable<Reminder> GenerateRemindersForPlant(Plant plant);
+    }
+} 
---
+++ PlantCareBuddy/PlantCareBuddy.Application/Services/StrategyBasedReminderService.cs
@@ -0,0 +1,77 @@
+﻿using PlantCareBuddy.Application.Interfaces;
+using PlantCareBuddy.Domain.Entities;
+using PlantCareBuddy.Domain.Enums;
+using PlantCareBuddy.Domain.Interfaces;
+using PlantCareBuddy.Domain.ValueObjects;
+
+namespace PlantCareBuddy.Application.Services
+{
+    public class StrategyBasedReminderService : IStrategyBasedReminderService
+    {
+        private readonly ICareStrategyService _careStrategyService;
+        private readonly ISeasonService _seasonService;
+
+        public StrategyBasedReminderService(
+            ICareStrategyService careStrategyService,
+            ISeasonService seasonService)
+        {
+            _careStrategyService = careStrategyService;
+            _seasonService = seasonService;
+        }
+
+        public IEnumerable<Reminder> GenerateRemindersForPlant(Plant plant)
+        {
+            var reminders = new List<Reminder>();
+            var strategy = _careStrategyService.GetStrategyForPlant(plant);
+            var season = _seasonService.GetCurrentSeason();
+
+            // Watering Reminder
+            int wateringInterval = strategy.GetWateringFrequencyDays(season);
+            if (wateringInterval > 0)
+            {
+                var nextWatering = DateTime.Today.AddDays(wateringInterval);
+                var wateringRecurrence = RecurrencePattern.Create(
+                    RecurrenceType.Daily, wateringInterval);
+
+                reminders.Add(Reminder.Create(
+                    plant.Id,
+                    ReminderType.Watering,
+                    "Watering",
+                    $"Water your {plant.Name}",
+                    nextWatering,
+                    wateringRecurrence,
+                    CareIntensity.Balanced,
+                    strategyId: null,
+                    strategyParameters: null,
+                    isStrategyOverride: false
+                ));
+            }
+
+            // Fertilizing Reminder
+            int fertilizingInterval = strategy.GetFertilizingFrequencyDays(season);
+            if (fertilizingInterval > 0)
+            {
+                var nextFertilizing = DateTime.Today.AddDays(fertilizingInterval);
+                var fertilizingRecurrence = RecurrencePattern.Create(
+                    RecurrenceType.Daily, fertilizingInterval);
+
+                reminders.Add(Reminder.Create(
+                    plant.Id,
+                    ReminderType.Fertilizing,
+                    "Fertilizing",
+                    $"Fertilize your {plant.Name}",
+                    nextFertilizing,
+                    fertilizingRecurrence,
+                    CareIntensity.Balanced,
+                    strategyId: null,
+                    strategyParameters: null,
+                    isStrategyOverride: false
+                ));
+            }
+
+            // Add more care types as needed (e.g., repotting, pruning, etc.)
+
+            return reminders;
+        }
+    }
+}
